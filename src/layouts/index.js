e32f5c83e6a31f87d66feeab45c7c5ba437fb15a
sinchang <sinchangwen@gmail.com>
2018-12-12 12:06:10 +0800

    Add project page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import 'normalize.css'
 import '../css/index.css'
 import 'prismjs/themes/prism-solarizedlight.css'
+
 export default ({ children }) => (
   <div style={{ margin: `0 auto`, maxWidth: 768, padding: `2rem 1rem` }}>
     {children()}
