66979a7db56753734c9704d63255d55207bb197b
Jeff Wen <3297859+sinchang@users.noreply.github.com>
2022-01-27 12:36:11 +0800

    Add page view (#243)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'next-themes'
+import { useAnalytics } from '@happykit/analytics'
 import { Header } from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useAnalytics({ publicKey: 'analytics_pub_dbf227c70c' })
   return (
     <>
       <Head>
