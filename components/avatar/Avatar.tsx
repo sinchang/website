63bee9b5c42c49c4dfcf40962770278db279209a
sinchang <sinchangwen@gmail.com>
2023-06-06 21:53:23 +0800

    fix build

--- components/avatar/Avatar.tsx
+++ components/avatar/Avatar.tsx
@@ -1,10 +1,12 @@
 
 interface AvatarProps extends React.ImgHTMLAttributes<HTMLImageElement> {}
 
-export const Avatar = ({className, ...props}: AvatarProps) => {
+export const Avatar = ({className,alt, ...props}: AvatarProps) => {
   return (
+      // eslint-disable-next-line @next/next/no-img-element
       <img
         className={`rounded-full ${className}`}
+        alt={alt}
         {...props}
       />
   )
