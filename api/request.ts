39c372f4dbc3bbc4d504173ca43e224914b5e8ec
sinchang <sinchangwen@gmail.com>
2023-06-26 21:45:43 +0800

    fix lint

--- api/request.ts
+++ api/request.ts
@@ -1,21 +1,19 @@
-import axios from "axios";
-import type { AxiosRequestConfig, AxiosError, AxiosResponse } from 'axios'
+import axios from 'axios'
+import type { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
 
 const instance = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
-  headers: {'X-Language': 'foobar'}
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  headers: { 'X-Language': 'foobar' },
 })
 
-
-export const request = async <D, R>(options: AxiosRequestConfig<D>) => {
+export async function request<D, R>(options: AxiosRequestConfig<D>) {
   const onSuccess = (response: AxiosResponse<R>) => {
     return response.data
-  };
+  }
 
-  const onError =  (error: AxiosError) => {
-    return Promise.reject(error.response);
-  };
+  const onError = (error: AxiosError) => {
+    return Promise.reject(error.response)
+  }
 
-  return instance(options).then(onSuccess).catch(onError);
-};
-
+  return instance(options).then(onSuccess).catch(onError)
+}
