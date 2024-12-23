3b0656f07dff0cb61e9a651fa5169370d5d86e29
sinchang <sinchangwen@gmail.com>
2024-12-23 13:46:07 +0800

    Link to google map

--- components/CheckIn.tsx
+++ components/CheckIn.tsx
@@ -26,10 +26,12 @@ export function CheckIn() {
 
   return checkInDetails?.venue
     ? <div>
-      <h1 className='pl-3 text-xl font-bold'>Last seen at:</h1>
-      <div className='relative my-4 block h-[300px] w-[300px] overflow-hidden rounded-3xl' >
-        <MapCard latitude={Number(checkInDetails.lat)} longitude={Number(checkInDetails.lng)} location={checkInDetails.location} />
-      </div>
+        <h1 className='pl-3 text-xl font-bold'>Last seen at:</h1>
+        <a href={`https://www.google.com/maps/place/${checkInDetails.lat}+${checkInDetails.lng}`}>
+          <div className='relative my-4 block h-[300px] w-[300px] overflow-hidden rounded-3xl' >
+            <MapCard latitude={Number(checkInDetails.lat)} longitude={Number(checkInDetails.lng)} location={checkInDetails.location} />
+          </div>
+        </a>
       </div>
     : null
 }
