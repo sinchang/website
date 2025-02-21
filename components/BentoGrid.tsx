e7533f34def69eeb50757d7c502262ecda39af97
sinchang <sinchangwen@gmail.com>
2025-02-21 15:07:20 +0800

    Update layout

--- components/BentoGrid.tsx
+++ components/BentoGrid.tsx
@@ -1,49 +1,53 @@
-import GridLayout, { WidthProvider } from 'react-grid-layout'
+import { WidthProvider, Responsive } from 'react-grid-layout'
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 import { CheckIn } from './CheckIn'
 
-const ReactGridLayout = WidthProvider(GridLayout)
+const ReactGridLayout = WidthProvider(Responsive)
 
-const layout = [
-  { i: 'a', x: 0, y: 0, w: 2, h: 2 },
-  { i: 'b', x: 3, y: 1, w: 2, h: 2 },
-]
+const layouts = {
+  lg: [
+    { i: 'a', x: 0, y: 0, w: 2, h: 2 },
+    { i: 'b', x: 3, y: 1, w: 2, h: 2 },
+  ]
+}
 
-export default function BentoGrid({ film, checkInDetails }: { film: {
-  image: string | undefined
-  uri: string
-  ratingText: string
-}, checkInDetails: {
-  venue: string
-  lat: string
-  lng: string
-  cc: string
-  location: string
-} }) {
+export default function BentoGrid({ film, checkInDetails }: {
+  film: {
+    image: string | undefined
+    uri: string
+    ratingText: string
+  }, checkInDetails: {
+    venue: string
+    lat: string
+    lng: string
+    cc: string
+    location: string
+  }
+}) {
   return (
-        <ReactGridLayout
-          layout={layout}
-          cols={4}
-          margin={[15, 15]}
-          rowHeight={150}
-          isDraggable={false}
-          isResizable={false}
-        >
-           <div key="a">
-            <CheckIn {...checkInDetails} />
-           </div>
-         <div key="b" className="relative h-full max-w-[624px] overflow-hidden rounded-3xl border p-6 shadow-md" style={{
-           backgroundColor: 'rgb(29, 24, 44)',
-           borderColor: 'rgb(64, 58, 85)',
-         }}>
-          <a href={film?.uri?.replace('sinchang', '')} target="_blank">
+    <ReactGridLayout
+      layouts={layouts}
+      cols={{ lg: 4, xs: 4, xxs: 1 }}
+      margin={[15, 15]}
+      rowHeight={150}
+      isDraggable={false}
+      isResizable={false}
+    >
+      <div key="a">
+        <CheckIn {...checkInDetails} />
+      </div>
+      <div key="b" className="relative h-full max-w-[624px] overflow-hidden rounded-3xl border p-6 shadow-md" style={{
+        backgroundColor: 'rgb(29, 24, 44)',
+        borderColor: 'rgb(64, 58, 85)',
+      }}>
+        <a href={film?.uri?.replace('sinchang', '')} target="_blank">
           <div className='flex h-full w-full flex-col items-center justify-between gap-4'>
-            <img src={film?.image} className="h-4/5"/>
+            <img src={film?.image} className="h-4/5" />
             <div>{film?.ratingText}</div>
           </div>
-          </a>
-         </div>
-        </ReactGridLayout>
+        </a>
+      </div>
+    </ReactGridLayout>
   )
 }
